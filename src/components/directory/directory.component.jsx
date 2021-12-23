604aa39bf84a4746b1230b5c6189d2022ae993f0
Scott Collier <scott.h.collier@gmail.com>
2021-12-23 18:22:48 +0000

    updating app with a shop page, collection preview component and collection item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,32 +12,37 @@ class Directory extends Component {
             sections: [
                 {
                     title: "hats",
-                    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+                    // imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imageUrl: "images/hats.png", // local file in public/images/
                     id: 1,
                     linkUrl: "shop/hats",
                 },
                 {
                     title: "jackets",
-                    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+                    // imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    imageUrl: "images/jackets.png", // local file in public/images/
                     id: 2,
                     linkUrl: "shop/jackets",
                 },
                 {
                     title: "sneakers",
-                    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+                    // imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    imageUrl: "images/sneakers.png", // local file in public/images/
                     id: 3,
                     linkUrl: "shop/sneakers",
                 },
                 {
                     title: "womens",
-                    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+                    // imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imageUrl: "images/womens.png", // local file in public/images/
                     size: "large",
                     id: 4,
                     linkUrl: "shop/womens",
                 },
                 {
                     title: "mens",
-                    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+                    // imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    imageUrl: "images/men.png", // local file in public/images/
                     size: "large",
                     id: 5,
                     linkUrl: "shop/mens",
@@ -49,13 +54,8 @@ class Directory extends Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem
-                        key={id}
-                        title={title}
-                        imageUrl={imageUrl}
-                        size={size}
-                    />
+                {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         );

44ba97fff4dc3fedf031a4c33a562eb0bfa8f9c5
Scott Collier <scott.h.collier@gmail.com>
2022-03-06 15:32:44 +0000

    added collection component and updated selectors, added routes for all categories

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Routes, Route, useMatch } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = () => (
-    <div className="shop-page">
-        <CollectionsOverview />
-    </div>
-);
+const ShopPage = () => {
+    const match = useMatch("/shop/:collectionId");
+
+    return (
+        <div className="shop-page">
+            <Routes>
+                <Route path="/" element={<CollectionsOverview />} />
+                <Route
+                    path=":collectionId"
+                    element={<CollectionPage match={match} />}
+                />
+            </Routes>
+        </div>
+    );
+};
 
 export default ShopPage;
